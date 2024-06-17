5e5b43607ac324f09044981655c94ccb1313975c
Diwash Karki <diwaskarki96@gmail.com>
2024-06-17 21:30:34 +0545

    cache cleanup

--- client/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import snackbarReducer from "./slices/snackbarSlice";
-import profilePictureReducer from "./slices/profilePictureSlice";
-export const store = configureStore({
-  reducer: {
-    snackbar: snackbarReducer,
-    profilePicture: profilePictureReducer,
-  },
-});
