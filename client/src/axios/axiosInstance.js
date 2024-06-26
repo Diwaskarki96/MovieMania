b04c6b8b0769d1ba80c4929e1fb120080fea64b2
Diwash Karki <diwaskarki96@gmail.com>
2024-06-26 22:43:10 +0545

    some changes

--- client/src/axios/axiosInstance.js
+++ client/src/axios/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 export const $axios = axios.create({
   baseURL: "https://movie-mania-api.vercel.app/",
-  timeout: 5000,
 });
 //axios request interceptor
 $axios.interceptors.request.use(function (config) {
